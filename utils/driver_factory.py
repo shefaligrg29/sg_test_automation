d047e06b0531796e8a656b1426d83a4f210f6cf7
shefali_garg <shefaligarg2909@gmail.com>
2024-08-30 22:32:28 +0530

    docker-execution fixed

--- utils/driver_factory.py
+++ utils/driver_factory.py
@@ -6,9 +6,11 @@ class DriverFactory:
     def get_driver(browser_name="chrome"):
         if browser_name == "chrome":
             options = webdriver.ChromeOptions()
-            # options.add_argument("--headless")  # Run in headless mode
+            options.add_argument("--headless")  # Run in headless mode
             options.add_argument('--no-sandbox')  # Required for Docker
             options.add_argument('--disable-dev-shm-usage')  # Required for Docker
+            options.add_argument("--disable-gpu")
+            options.add_argument("--window-size=1920x1080")
             driver = webdriver.Chrome(options=options)
         else:
             raise ValueError("Browser not supported")
